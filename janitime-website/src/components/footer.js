2a39044df77bcf8720df414777f6eafe5191d289
Michael VanSleen <mike.vansleen@gmail.com>
2019-11-26 12:13:24 -0700

    Footer styled for desktop

---
+++ janitime-website/src/components/footer.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import janitimeLogo from "../images/janitime-logo.svg"
+import telephone from "../images/phone-icon.svg"
+import mail from "../images/mail-icon.svg"
+
+const Footer = ({ siteTitle }) => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="logo-container">
+          <div className="logo">
+            <Link to="/">
+              <img src={janitimeLogo} alt="Janitime" />
+            </Link>
+          </div>
+          <div className="logo-text">
+            <Link to="/">{siteTitle}</Link>
+          </div>
+          <div className="footer-links">
+            <h3>Janitime</h3>
+            <Link to="/">Features</Link>
+            <Link to="/">Getting Started</Link>
+            <Link to="/">Pricing</Link>
+            <Link to="/">Sign Up</Link>
+          </div>
+        </div>
+        <div className="footer-contact-info">
+          <h3>Contact Us</h3>
+          <img src={telephone} alt="telephone icon" />
+          <span>
+            <p>616-894-1834</p>
+          </span>
+          <br />
+          <br />
+          <p>or</p>
+          <br />
+          <br />
+          <img src={mail} alt="mail icon" />
+          <span>
+            <p>sales@janitime.com</p>
+          </span>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- janitime-website/src/pages/index.js
+++ janitime-website/src/pages/index.js
@@ -8,12 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Your Janitorial Management System</h1>
+    <p>
+      No more missed cleanings, padded timesheets, calucation errors or paper
+      inspections.
+    </p>
+    <button>Sign Up</button>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
