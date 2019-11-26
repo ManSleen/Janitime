0c11b286b3468577d0a0ac67de2d6bc0e49b571e
Michael VanSleen <mike.vansleen@gmail.com>
2019-11-26 14:20:33 -0700

    hero section styled

--- janitime-website/src/pages/index.js
+++ janitime-website/src/pages/index.js
@@ -2,20 +2,56 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import illustration from "../images/illustration1.svg"
+import easySetup from "../images/easy-setup.svg"
+import computer from "../images/computer-icon.svg"
+import wallet from "../images/wallet.svg"
+import chart from "../images/chart.svg"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Your Janitorial Management System</h1>
-    <p>
-      No more missed cleanings, padded timesheets, calucation errors or paper
-      inspections.
-    </p>
-    <button>Sign Up</button>
-
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="landing-container">
+      <section className="hero split">
+        <div className="left">
+          <h1>Your Janitorial Management System</h1>
+          <p>
+            No more missed cleanings, padded timesheets, calucation errors or
+            paper inspections.
+          </p>
+          <button className="hero-signup">
+            <Link to="/page-2/">Sign Up</Link>
+          </button>
+        </div>
+        <div className="right">
+          <img src={illustration} alt="janitime" />
+        </div>
+      </section>
+    </div>
+    <section className="learn-more-container">
+      <div className="learn-more">
+        <div className="benefit-container">
+          <p>Easy setup</p>
+          <div className="benefit">
+            <img src={computer} />
+          </div>
+        </div>
+        <div className="benefit-container">
+          <p>Intuitive interface</p>
+          <div className="benefit">
+            <img src={chart} />
+          </div>
+        </div>
+        <div className="benefit-container">
+          <p>Affordable</p>
+          <div className="benefit">
+            <img src={wallet} />
+          </div>
+        </div>
+      </div>
+      <button className="learn-more-button">Learn More</button>
+    </section>
   </Layout>
 )
 
